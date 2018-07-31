f7e9c798e42f5e212587dd18e2b0d0ef15920e00
Michelle Levine <michelle@michellejl.com>
2018-07-30 22:30:58 -0700

    connecting to contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography.js`,
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `sr7yehvxvava`,
+        accessToken: `3bafa9e1e8810963d5adc8a372d1d911bbf8951939b99d649972e808754d8ee7`,
+      },
+    },
   ],
 }
